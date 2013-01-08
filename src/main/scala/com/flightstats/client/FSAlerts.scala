f9146a4b8f936ecf24ede890cff004464610f7c7
Jeff Lavallee <jeff@zeroclue.com>
2013-01-07 22:49:16 -0800

    added optional args to alerts methods
    NOTE: Did *not* add args to methods that only accept extendedOptions.
          Still debating about whether or not to allow them to be
          overridden, or perhaps enforce that useHttpErrors is set?

--- src/main/scala/com/flightstats/client/FSAlerts.scala
+++ src/main/scala/com/flightstats/client/FSAlerts.scala
@@ -15,46 +15,46 @@ abstract class FSAlerts(val appId: String, val appKey: String) extends FSClient 
 
   // https://api.flightstats.com/flex/alerts/rest/v1/json
   def apiLocation = Seq("flex", "alerts", "rest", "v1", "json")
-
+  
   /* Create flight rule by arrival
    * /v1/json/create/{carrier}/{flightNumber}/to/{arrivalAirport}/arriving/{year}/{month}/{day} GET
    */
-  def createRuleByArrival(deliverTo: String, carrier: String, flightNumber: String, arrivalAirport: String, date: DateTime) =
+  def createRuleByArrival(deliverTo: String, carrier: String, flightNumber: String, arrivalAirport: String, date: DateTime, args: ArgMap = Map.empty): Promise[FSAlert] =
     // TODO: allow type to be configurable
     alert( url / "create" / carrier / flightNumber / "to" / arrivalAirport / "arriving" / date
-        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON"))
+        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON") ++ args)
 
   /* Create flight rule by departure
    * /v1/json/create/{carrier}/{flightNumber}/from/{departureAirport}/departing/{year}/{month}/{day} GET
    */
-  def createRuleByDeparture(deliverTo: String, carrier: String, flightNumber: String, departureAirport: String, date: DateTime) =
+  def createRuleByDeparture(deliverTo: String, carrier: String, flightNumber: String, departureAirport: String, date: DateTime, args: ArgMap = Map.empty): Promise[FSAlert] =
     alert( url / "create" / carrier / flightNumber / "from" / departureAirport / "departing" / date
-        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON"))
+        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON") ++ args)
 
   /* Create flight rule for route with arrival date
    * /v1/json/create/{carrier}/{flightNumber}/from/{departureAirport}/to/{arrivalAirport}/arriving/{year}/{month}/{day} GET
    */
-  def createRuleForRouteByArrivalDate(deliverTo: String, carrier: String, flightNumber: String, departureAirport: String, arrivalAirport: String, date: DateTime) =
+  def createRuleForRouteByArrivalDate(deliverTo: String, carrier: String, flightNumber: String, departureAirport: String, arrivalAirport: String, date: DateTime, args: ArgMap = Map.empty): Promise[FSAlert] =
     alert( url / "create" / carrier / flightNumber / "from" / departureAirport / "to" / arrivalAirport / "arriving" / date
-        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON"))
+        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON") ++ args)
 
   /* Create flight rule for route with departure date
    * /v1/json/create/{carrier}/{flightNumber}/from/{departureAirport}/to/{arrivalAirport}/departing/{year}/{month}/{day} GET
    */
-  def createRuleForRouteByDepartureDate(deliverTo: String, carrier: String, flightNumber: String, departureAirport: String, arrivalAirport: String, date: DateTime) =
+  def createRuleForRouteByDepartureDate(deliverTo: String, carrier: String, flightNumber: String, departureAirport: String, arrivalAirport: String, date: DateTime, args: ArgMap = Map.empty): Promise[FSAlert] =
     alert( url / "create" / carrier / flightNumber / "from" / departureAirport / "to" / arrivalAirport / "departing" / date
-        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON"))
+        <<? Map("deliverTo" -> deliverTo, "type" -> "JSON") ++ args)
 
   /* Deletes a previously registered rule by ID
    * /v1/json/delete/{ruleId} GET
    */
-  def delete(ruleId: Integer) =
+  def delete(ruleId: Integer): Promise[FSAlert] =
     alert(url / "delete" / ruleId.toString())
 
   /* Retrieve a previously registered rule by ID
    * /v1/json/get/{ruleId} GET
    */
-  def get(ruleId: Integer) =
+  def get(ruleId: Integer): Promise[FSAlert] =
     alert(url / "get" / ruleId.toString())
 
 
---
+++ src/main/scala/com/flightstats/client/FSError.scala
@@ -0,0 +1,12 @@
+package com.flightstats.client.api.v1
+
+import java.util.UUID
+import com.fasterxml.jackson.annotation.{JsonCreator, JsonProperty}
+
+@JsonCreator
+case class FSError(
+  @JsonProperty("httpStatusCode") httpStatusCode: Integer,
+  @JsonProperty("errorCode") errorCode: String, // TODO: make an enum for these
+  @JsonProperty("errorId") errorId: UUID,
+  @JsonProperty("errorMessage") errorMessage: String
+)
