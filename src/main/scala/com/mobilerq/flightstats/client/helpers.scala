0e11a1de7b2751956f60d73ae33bdcbbb09e8221
Jeff Lavallee <jeff@zeroclue.com>
2013-04-01 14:25:25 -0700

    do *not* return Rich response types by default
    users can force conversion by specifying that they
    desire a rich response type explicitly

--- src/main/scala/com/mobilerq/flightstats/client/helpers.scala
+++ src/main/scala/com/mobilerq/flightstats/client/helpers.scala
@@ -5,17 +5,17 @@ import dispatch.Promise
 import com.mobilerq.flightstats.api.v1.flightstatus._
 
 trait FSFlightStatusHelpers extends FSClient {
-  protected def status(url: RequestBuilder): Promise[RichFlightStatusResponse]  =
+  protected def status(url: RequestBuilder): Promise[FSFlightStatusResponse]  =
     getAndDeserialize(classOf[FSFlightStatusResponse], url)
 
-  protected def statuses(url: RequestBuilder): Promise[RichFlightStatusesResponse] =
+  protected def statuses(url: RequestBuilder): Promise[FSFlightStatusesResponse] =
     getAndDeserialize(classOf[FSFlightStatusesResponse], url)
 }
 
 trait FSFlightTrackHelpers extends FSClient {
-  protected def track(url: RequestBuilder): Promise[RichFlightTrackResponse] =
+  protected def track(url: RequestBuilder): Promise[FSFlightTrackResponse] =
     getAndDeserialize(classOf[FSFlightTrackResponse], url)
 
-  protected def tracks(url: RequestBuilder): Promise[RichFlightTracksResponse] =
+  protected def tracks(url: RequestBuilder): Promise[FSFlightTracksResponse] =
     getAndDeserialize(classOf[FSFlightTracksResponse], url)
 }
